9b1ad7af6c68d422c92bd77e7337dd59a2b01fe2
Vladyslav Hasporian <salasengland33@gmail.com>
2025-06-21 11:38:41 +0300

    add translation-service

---
+++ translation-service/src/test/java/com/salas/translation/service/TranslationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salas.translation.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
