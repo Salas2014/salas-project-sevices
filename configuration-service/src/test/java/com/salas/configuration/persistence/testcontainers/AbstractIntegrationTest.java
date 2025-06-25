b70efc3ba3b069a42556305ae2dff74dcbdcb9e3
Vladyslav Hasporian <salasengland33@gmail.com>
2025-06-25 16:14:12 +0300

    add testcontainers for configuration service

---
+++ configuration-service/src/test/java/com/salas/configuration/persistence/testcontainers/AbstractIntegrationTest.java
@@ -0,0 +1,39 @@
+package com.salas.configuration.persistence.testcontainers;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AbstractIntegrationTest {
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:16-alpine"
+    );
+
+    @BeforeAll
+    static void startPostgres() {
+        postgres.start();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        postgres.stop();
+    }
+
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+}
---
+++ configuration-service/src/test/java/com/salas/configuration/persistence/testcontainers/UserControllerTest.java
@@ -0,0 +1,51 @@
+package com.salas.configuration.persistence.testcontainers;
+
+import com.salas.configuration.entities.User;
+import com.salas.configuration.repository.UserRepo;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
+
+
+public class UserControllerTest extends AbstractIntegrationTest {
+    @LocalServerPort
+    private Integer port;
+
+    @Autowired
+    public UserRepo userRepo;
+
+    @BeforeEach
+    public void setup() {
+        RestAssured.baseURI = "http://localhost:" + port;
+    }
+
+    @BeforeEach
+    void setUp() {
+
+        userRepo.deleteAll();
+    }
+
+    @Test
+    public void shutGetAllUsers() {
+        User vlad = new User("Vlad", "salas@vk.kh.ua", 380632836733L);
+        User sergey = new User("Sergey", "sergey@vk.kh.ua", 380632836733L);
+
+        userRepo.saveAll(List.of(vlad, sergey));
+
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/v1/users")
+                .then()
+                .statusCode(200)
+                .body(".", hasSize(2));
+    }
+}
