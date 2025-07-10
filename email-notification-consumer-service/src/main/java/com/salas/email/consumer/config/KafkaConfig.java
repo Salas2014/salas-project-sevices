fa0578ec15b3971586c869745e5808e93680382c
Vladyslav Hasporian <salasengland33@gmail.com>
2025-07-10 22:56:35 +0300

    [Consumer]: deserialization Exception wrapper

--- email-notification-consumer-service/src/main/java/com/salas/email/consumer/config/KafkaConfig.java
+++ email-notification-consumer-service/src/main/java/com/salas/email/consumer/config/KafkaConfig.java
@@ -9,6 +9,7 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
@@ -23,11 +24,11 @@ public class KafkaConfig {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092,localhost:39094");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
+
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "product-created-consumer");
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.salas.common.events");
-
-
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
@@ -37,10 +38,8 @@ public class KafkaConfig {
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
 
-        // Настройки контейнера
-        factory.setConcurrency(3); // количество потоков
+        factory.setConcurrency(3);
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
-
         return factory;
     }
 }
